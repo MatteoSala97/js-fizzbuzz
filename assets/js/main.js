f66849af56db7affb763d6b77a86502789080e77
MatteoSala97 <matteo111997@gmail.com>
2024-01-09 16:27:10 +0100

    minor fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,33 @@
 //Declaring const list-group class in html
-const ul = document.querySelector("ul.list-group")
+const ul = document.getElementById("fizzbuzz-list");
 
 //Declating variables 
 let fizz = "Fizz"
 let buzz = "Buzz"
 
+
 //cycle for numbers from 1 to 100 
 for ( let i = 1; i<= 100; i++) {
-    if ((( i % 3 ) === 0) && (( i % 5 ) === 0)){
+    let item = ""
+    
+    if (i % 3 === 0 && i % 5 === 0){
         console.log (fizz+buzz)
+        // item = `<li class="list-group-item"> ${fizz + buzz} </li>`
     }
-    else if (( i % 3 ) === 0) {
+    else if ( i % 3  === 0) {
         console.log(fizz)
+        // item = `<li class="list-group-item"> ${fizz}</li>`
     }
-    else if (( i % 5 ) === 0){
+    else if ( i % 5  === 0){
         console.log(buzz)
+        // item = `<li class="list-group-item"> ${buzz}</li>`
     }   
     else 
     console.log (i)
+    // item = `<li class="list-group-item">${i}</li>`
+    
+    ul.innerHTML += item;
 }
+
+
 
