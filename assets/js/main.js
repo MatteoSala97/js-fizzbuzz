9e99468e5665b434fbf1496b8bf3ae4119977504
MatteoSala97 <matteo111997@gmail.com>
2024-01-10 16:43:25 +0100

    corrected exercise

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,30 +4,30 @@ const ul = document.getElementById("fizzbuzz-list");
 //Declating variables 
 let fizz = "Fizz"
 let buzz = "Buzz"
+let variante = "Fizzbuzz"
 
 
-//cycle for numbers from 1 to 100 
-for ( let i = 1; i<= 100; i++) {
-    let item = ""
-    
+const gridHtml = document.getElementById ("grid")                           //aggiunto a lezione
+console.log(gridHtml)                                                       //aggiunto a lezione
+
+
+//cycle for numbers from 1 to 100
+for ( let i = 1; i <= 100; i++){
     if (i % 3 === 0 && i % 5 === 0){
-        console.log (fizz+buzz)
-        // item = `<li class="list-group-item"> ${fizz + buzz} </li>`
+        gridHtml.innerHTML += `<div class="box box-red">${variante}</div>`  //aggiunto a lezione
+        console.log (variante) 
     }
-    else if ( i % 3  === 0) {
-        console.log(fizz)
-        // item = `<li class="list-group-item"> ${fizz}</li>`
+    else if ( i % 3  === 0){
+        gridHtml.innerHTML += `<div class="box box-green">${fizz}</div>`    //aggiunto a lezione
+        console.log(fizz) 
     }
     else if ( i % 5  === 0){
+        gridHtml.innerHTML += `<div class="box box-yellow">${buzz}</div>`   //aggiunto a lezione
         console.log(buzz)
-        // item = `<li class="list-group-item"> ${buzz}</li>`
     }   
     else 
+    gridHtml.innerHTML += `<div class="box">${variante}</div>`              //aggiunto a lezione
     console.log (i)
-    // item = `<li class="list-group-item">${i}</li>`
-    
-    ul.innerHTML += item;
 }
-
 
 
